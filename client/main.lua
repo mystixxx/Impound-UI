440951a2838bbd1725985fc768f206a88ab7b447
mystixxx <77647824+mystixxx@users.noreply.github.com>
2021-02-21 22:21:54 +0100

    Faster Native
    PlayerPedId() is faster than GetPlayerPed(-1)

--- client/main.lua
+++ client/main.lua
@@ -1,13 +1,15 @@
+
+
 local Keys = {
-    ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-    ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-    ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-    ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-    ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-    ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-    ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-    ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-    ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
 }
 
 
@@ -15,16 +17,16 @@ ESX = nil
 local PlayerData = {}
 
 Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
-    end
+  while ESX == nil do
+      TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+      Citizen.Wait(0)
+  end
 
-    while ESX.GetPlayerData().job == nil do
-        Citizen.Wait(10)
-    end
+  while ESX.GetPlayerData().job == nil do
+      Citizen.Wait(10)
+  end
 
-    PlayerData = ESX.GetPlayerData()
+  PlayerData = ESX.GetPlayerData()
 end)
 
 local garages = {
@@ -38,179 +40,179 @@ local garages = {
 local enableField = false
 
 function AddCar(plate)
-    SendNUIMessage({
-        action = 'add',
-        plate = plate
-    }) 
+  SendNUIMessage({
+      action = 'add',
+      plate = plate
+  }) 
 end
 
 function toggleField(enable)
-    SetNuiFocus(enable, enable)
-    enableField = enable
+  SetNuiFocus(enable, enable)
+  enableField = enable
 
-    if enable then
-        SendNUIMessage({
-            action = 'open'
-        }) 
-    else
-        SendNUIMessage({
-            action = 'close'
-        }) 
-    end
+  if enable then
+      SendNUIMessage({
+          action = 'open'
+      }) 
+  else
+      SendNUIMessage({
+          action = 'close'
+      }) 
+  end
 end
 
 
 
 AddEventHandler('onResourceStart', function(name)
-    if GetCurrentResourceName() ~= name then
-        return
-    end
+  if GetCurrentResourceName() ~= name then
+      return
+  end
 
-    toggleField(false)
+  toggleField(false)
 
 
 end)
 
 RegisterNUICallback('escape', function(data, cb)
-    toggleField(false)
-    SetNuiFocus(false, false)
+  toggleField(false)
+  SetNuiFocus(false, false)
 
-    cb('ok')
+  cb('ok')
 end)
 
 RegisterNUICallback('enable-parkout', function(data, cb)
-    
-    ESX.TriggerServerCallback('fd_impound:loadVehicles', function(vehicles)
-        for key, value in pairs(vehicles) do
-            AddCar(value.plate)
-        end
-    end)
-    
-    cb('ok')
+  
+  ESX.TriggerServerCallback('fd_impound:loadVehicles', function(vehicles)
+      for key, value in pairs(vehicles) do
+          AddCar(value.plate)
+      end
+  end)
+  
+  cb('ok')
 end) 
 
 RegisterNUICallback('enable-parking', function(data, cb)
-    
-    local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(GetPlayerPed(-1)), 25.0)
+  
+  local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(PlayerPedId()), 25.0)
 
-    for key, value in pairs(vehicles) do
-        ESX.TriggerServerCallback('fd_impound:isOwned', function(owned)
+  for key, value in pairs(vehicles) do
+      ESX.TriggerServerCallback('fd_impound:isOwned', function(owned)
 
-            if owned then
-                AddCar(GetVehicleNumberPlateText(value))
-            end
-    
-        end, GetVehicleNumberPlateText(value))
-    end
-    
-    cb('ok')
+          if owned then
+              AddCar(GetVehicleNumberPlateText(value))
+          end
+  
+      end, GetVehicleNumberPlateText(value))
+  end
+  
+  cb('ok')
 end) 
 
 local usedGarage
 
 RegisterNUICallback('park-out', function(data, cb)
-    
-    ESX.TriggerServerCallback('fd_impound:loadVehicle', function(vehicle)
-        local x,y,z = table.unpack(garages[usedGarage][2])
-        local props = json.decode(vehicle[1].vehicle)
+  
+  ESX.TriggerServerCallback('fd_impound:loadVehicle', function(vehicle)
+      local x,y,z = table.unpack(garages[usedGarage][2])
+      local props = json.decode(vehicle[1].vehicle)
 
-    local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(GetPlayerPed(-1)), 25.0)
+  local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(PlayerPedId()), 25.0)
 
-    for key, value in pairs(vehicles) do
-        if GetVehicleNumberPlateText(value) == data.plate then
-            ESX.Game.DeleteVehicle(value)
-        end
-    end
+  for key, value in pairs(vehicles) do
+      if GetVehicleNumberPlateText(value) == data.plate then
+          ESX.Game.DeleteVehicle(value)
+      end
+  end
 
 
-        ESX.Game.SpawnVehicle(props.model, {
-            x = x,
-            y = y,
-            z = z + 1
-        }, garages[usedGarage][3], function(callback_vehicle)
-            ESX.Game.SetVehicleProperties(callback_vehicle, props)
-            SetVehRadioStation(callback_vehicle, "OFF")
-            TaskWarpPedIntoVehicle(GetPlayerPed(-1), callback_vehicle, -1)
-        end)
+      ESX.Game.SpawnVehicle(props.model, {
+          x = x,
+          y = y,
+          z = z + 1
+      }, garages[usedGarage][3], function(callback_vehicle)
+          ESX.Game.SetVehicleProperties(callback_vehicle, props)
+          SetVehRadioStation(callback_vehicle, "OFF")
+          TaskWarpPedIntoVehicle(PlayerPedId(), callback_vehicle, -1)
+      end)
 
-    end, data.plate)
+  end, data.plate)
 
-    TriggerServerEvent('fd_impound:changeState', data.plate, 1)
-    
-    cb('ok')
+  TriggerServerEvent('fd_impound:changeState', data.plate, 1)
+  
+  cb('ok')
 end)
 
 RegisterNUICallback('park-in', function(data, cb)
-    
-    local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(GetPlayerPed(-1)), 25.0)
+  
+  local vehicles = ESX.Game.GetVehiclesInArea(GetEntityCoords(PlayerPedId()), 25.0)
 
-    for key, value in pairs(vehicles) do
-        if GetVehicleNumberPlateText(value) == data.plate then
-            TriggerServerEvent('fd_impound:saveProps', data.plate, ESX.Game.GetVehicleProperties(value))
-            TriggerServerEvent('fd_impound:changeState', data.plate, 1)
-            ESX.Game.DeleteVehicle(value)
-        end
-    end
+  for key, value in pairs(vehicles) do
+      if GetVehicleNumberPlateText(value) == data.plate then
+          TriggerServerEvent('fd_impound:saveProps', data.plate, ESX.Game.GetVehicleProperties(value))
+          TriggerServerEvent('fd_impound:changeState', data.plate, 1)
+          ESX.Game.DeleteVehicle(value)
+      end
+  end
 
-    cb('ok')
+  cb('ok')
 end)
 
 Citizen.CreateThread(function()
-    while true do
-        Wait(0)
+  while true do
+      Wait(0)
 
-        for key, value in pairs(garages) do
-            local dist = GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), value[1])
+      for key, value in pairs(garages) do
+          local dist = GetDistanceBetweenCoords(GetEntityCoords(PlayerPedId()), value[1])
 
-            if dist <= 2.0 then
-                ESX.ShowHelpNotification("Drücke ~INPUT_CONTEXT~ um auf dem Abschlepphof zuzugreifen")
+          if dist <= 2.0 then
+              ESX.ShowHelpNotification("Drücke ~INPUT_CONTEXT~ um auf dem Abschlepphof zuzugreifen")
 
-                if IsControlJustReleased(0, 38) then
-                    toggleField(true)
-                    usedGarage = key
-                end
-            end
-        end
-     end
+              if IsControlJustReleased(0, 38) then
+                  toggleField(true)
+                  usedGarage = key
+              end
+          end
+      end
+   end
 end)
 
 local coordinate = {
-    { 408.1, -1625.15, 29.29, nil, 225.00, nil, -1176698112}, 
-    { -174.9, -1273.35, 32.57, nil, 445.00, nil, 68070371},
-    { 1508.39, 3768.63, 34.14, nil, 286.16, nil, 68070371},
-    { -234.91, 6199.07, 31.94, nil, 245.0, nil, 68070371}                      
+  { 408.1, -1625.15, 29.29, nil, 225.00, nil, -1176698112}, 
+  { -174.9, -1273.35, 32.57, nil, 445.00, nil, 68070371},
+  { 1508.39, 3768.63, 34.14, nil, 286.16, nil, 68070371},
+  { -234.91, 6199.07, 31.94, nil, 245.0, nil, 68070371}                      
 }
 
 Citizen.CreateThread(function()
 
-    for _, v in pairs(coordinate) do
-        RequestModel(v[7])
-        while not HasModelLoaded(v[7]) do
-            Wait(1)
-        end
+  for _, v in pairs(coordinate) do
+      RequestModel(v[7])
+      while not HasModelLoaded(v[7]) do
+          Wait(1)
+      end
 
-        ped = CreatePed(4, v[7], v[1], v[2], v[3] - 1, 3374176, false, true)
-        SetEntityHeading(ped, v[5])
-        FreezeEntityPosition(ped, true)
-        SetEntityInvincible(ped, true)
+      ped = CreatePed(4, v[7], v[1], v[2], v[3] - 1, 3374176, false, true)
+      SetEntityHeading(ped, v[5])
+      FreezeEntityPosition(ped, true)
+      SetEntityInvincible(ped, true)
 
-        SetBlockingOfNonTemporaryEvents(ped, true)
-    end
+      SetBlockingOfNonTemporaryEvents(ped, true)
+  end
 
 end)
 
 Citizen.CreateThread(function()
-    for _, coords in pairs(garages) do
-        local blip = AddBlipForCoord(coords[1])
+  for _, coords in pairs(garages) do
+      local blip = AddBlipForCoord(coords[1])
 
-        SetBlipSprite(blip, 67)
-        SetBlipScale(blip, 0.9)
-        SetBlipColour(blip, 0)
-        SetBlipDisplay(blip, 4)
-        SetBlipAsShortRange(blip, true)
+      SetBlipSprite(blip, 67)
+      SetBlipScale(blip, 0.9)
+      SetBlipColour(blip, 0)
+      SetBlipDisplay(blip, 4)
+      SetBlipAsShortRange(blip, true)
 
-        BeginTextCommandSetBlipName("STRING")
-        AddTextComponentString("Garage - Abschlepphof")
-        EndTextCommandSetBlipName(blip)
-    end
+      BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString("Garage - Abschlepphof")
+      EndTextCommandSetBlipName(blip)
+  end
 end)
